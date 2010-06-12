2892b9523341de3c1b2fedf3353225ffe3599329
Wiennat Mongkulmann <wien@yavin.local>
2010-06-12 16:34:18 +0900

    add before create callback to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,23 @@
 class Event < ActiveRecord::Base
+  #has_many :questions
+  attr_accessor :questions_text
+  before_create :convert_text_to_questions
+  before_create :convert_text_to_questions2  
+  protected
+  def convert_text_to_questions
+    self.questions = @questions_text
+    @questions_text.each do |e|
+      puts "#{e} asdfasdf"
+    end
+  end
+
+  def convert_text_to_questions2
+    self.questions = @questions_text
+    @questions_text.each do |e|
+      puts "#{e}"
+    end
+  end
+  
 	def self.processQuestions
 	
 	end
