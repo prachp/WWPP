4e485e0adbd9765ce25dba557c898a3014769ac6
wiennat <wiennat@gmail.com>
2010-06-13 11:58:25 +0900

    simplify convert_text_to_question

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,32 +2,13 @@ class Event < ActiveRecord::Base
   has_many :questions
   has_many :attendees
   attr_accessor :questions_text
-  #before_create :convert_text_to_questions
-  #before_create :convert_text_to_questions2  
-  after_create :convert_text_to_questions3
+  after_create :convert_text_to_questions
+
   protected
   def convert_text_to_questions
-    self.questions = @questions_text
-    @questions_text.each do |e|
-      puts "#{e} asdfasdf"
-    end
-  end
-
-  def convert_text_to_questions2
-    self.questions = @questions_text
-    @questions_text.each do |e|
-      puts "#{e}"
-    end
-  end
-  
-  def convert_text_to_questions3
     @questions_text = self.questions_text.split(/\n/)
     @questions_text.each do |text|
-      if(!text.empty? && text!='\r')
-       q = Question.new
-       q.text = text
-       q.event_id = self.id
-       q.save
+      self.questions.create(:text => text) unless text.strip.empty?
      end
     end
   end
