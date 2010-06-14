5e8c1ecd18636e9781336d3e0b04d0f5c4cf066e
Prach Phalabhiromya <prach25@gmail.com>
2010-06-14 00:04:27 -0700

    add a lot of stuffs, please take a look :D

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -2,9 +2,20 @@ class AttendeesController < ApplicationController
 
   def create
     @attendee = Attendee.new(params[:attendee])
-    @attendee.save
-    @event =  Event.where(:id => @attendee.event_id)
-    redirect_to :action => "edit", :id => @attendee.id
+    if @attendee.save
+      @event =  Event.find(@attendee.event_id)
+      # Create answers
+      questions = Question.where(:event_id => @event.id)
+      puts questions
+      puts 'event.questions'
+      puts @event.questions
+      @event.questions.each do |q|
+        answer = Answer.new(:question_id => q.id, :attendee_id => @attendee.id, :answer => 1)
+        answer.save
+      end
+
+      redirect_to :action => "edit", :id => @attendee.id
+    end
   end
   
   def edit
