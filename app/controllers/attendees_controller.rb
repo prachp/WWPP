9402a51458c0df0eeadc6e728ce6cf57878c8c3c
wiennat <wiennat@gmail.com>
2010-06-15 01:12:38 +0900

    Change attendance controller behavior

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -2,25 +2,26 @@ class AttendeesController < ApplicationController
 
   def create
     @attendee = Attendee.new(params[:attendee])
-    if @attendee.save
-      @event =  Event.find(@attendee.event_id)
-      # Create answers
-      questions = Question.where(:event_id => @event.id)
-      puts questions
-      puts 'event.questions'
-      puts @event.questions
-      @event.questions.each do |q|
-        answer = Answer.new(:question_id => q.id, :attendee_id => @attendee.id, :answer => 1)
-        answer.save
-      end
-
-      redirect_to :action => "edit", :id => @attendee.id
-    end
+    @attendee.save
+    redirect_to :action=>:edit, :id=>@attendee.id
   end
-  
+
   def edit
     @attendee = Attendee.find(params[:id])
-    @event = Event.find(@attendee.event_id)
+    @event = Event.find(@attendee.event.id)
   end
-
+  
+  def update
+    @attendee = Attendee.find(params[:id])
+    respond_to do |format|
+      if @attendee.update_attributes(params[:attendee])
+        format.html { redirect_to(@attendee.event, :notice => 'Attendance was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @attendee.errors, :status => :unprocessable_entity }
+      end
+    end
+    
+  end
 end
