e7c54991bf4b0e14422d5b63f41c3337f3f32faf
wiennat <wiennat@gmail.com>
2010-06-16 01:52:13 +0900

    Move some logic from view into model

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,4 +1,13 @@
 class Attendee < ActiveRecord::Base
   has_many :answers 
   belongs_to :event
+  
+  def answer_for(question)
+    logger.info "question #{question.id}"
+    @answer = self.answers.find(:first, :conditions => {:question_id => question.id})
+    unless @answer
+      @answer = self.answers.create(:question_id=>question.id)
+    end
+    @answer
+  end
 end
