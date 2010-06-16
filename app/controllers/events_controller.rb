b333f321f8bdbda216391d3419305f9decac87ab
Prach Phalabhiromya <prach25@gmail.com>
2010-06-15 19:10:52 -0700

    showing signs instead of numbers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,7 +14,8 @@ class EventsController < ApplicationController
   # GET /events/1.xml
   def show
     @event = Event.find(params[:id])
-
+    @signs = ['-','X','△','O']
+    
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @event }
